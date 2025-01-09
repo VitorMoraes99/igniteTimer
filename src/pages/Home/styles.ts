d0877112b1d40ba79335eeb4ca46a9a95318d7cb
Vitor Matheus de Moraes <vitor.matheusm@gmail.com>
2025-01-08 23:00:42 -0300

    feat: finalizando as estilizações do home e do history, realizando as validações do form

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -24,6 +24,7 @@ export const FormContainer = styled.main `
     justify-content: center;
     gap: 0.5rem;
     color: ${(props) => props.theme['gray-100']};
+    font-size: 1.125rem;
     font-weight: bold;
     flex-wrap: wrap;
 `;
@@ -35,16 +36,32 @@ const BaseInput = styled.input `
     border-bottom: 2px solid ${(props) => props.theme['gray-500']};
     font-weight: bold;
     font-size: 1.125rem;
-    padding: 0 0.25rem;
-    color: ${(props) => props.theme['gray-100']}
+    padding: 0 0.5rem;
+    color: ${(props) => props.theme['gray-100']};
+    width: 47%;
+
+
+
+    &:focus {
+        box-shadow: none;
+        border-color: ${(props) => props.theme['green-500']};
+    }
+
+    &::placeholder {
+        color: ${(props) => props.theme['gray-500']};
+    }
 `;
 
 export const TaskInput = styled(BaseInput) `
     flex: 1;
+
+    &::-webkit-calendar-picker-indicator {
+        display: none !important;
+    }
 `;
 
 export const MinutesAmountInput = styled(BaseInput) `
-    width: 4rem;
+    width: 4rem; 
 `;
 
 export const CountdownContainer = styled.main `
